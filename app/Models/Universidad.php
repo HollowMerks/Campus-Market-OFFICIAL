a383f88756861edb9f69bae9285a80dad3c09881
Merks Hollow <jhoellaura1416@gmail.com>
2025-10-05 18:42:16 -0400

    migracioneshechas

--- app/Models/Universidad.php
+++ app/Models/Universidad.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+=======
+>>>>>>> 9835dc8 (migracioneshechas)
 use Illuminate\Database\Eloquent\Model;
 
 class Universidad extends Model
 {
+<<<<<<< HEAD
     use HasFactory;
 
     // Columnas que se pueden llenar en masa
@@ -16,3 +20,7 @@ class Universidad extends Model
     ];
 }
 
+=======
+    //
+}
+>>>>>>> 9835dc8 (migracioneshechas)
